a0cf13504ecd5d62357194d8cec486b0c483f1d6
kieran-wright <kieranwright@me.com>
2016-03-23 12:37:00 +0000

    Now automatically generates imsmanifest file and zip using gulp publish and gulp zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,28 @@
 // simple single SCO package 
 var gulp = require('gulp');
 var manifest = require('gulp-scorm-manifest');
- 
+var zip = require('gulp-zip');
+
+
 gulp.task('publish', function() {
-    gulp.src('course/**')
-    .pipe(manifest({
-      version: '1.2',
-      courseId: 'Test course',
-      SCOtitle: 'Test module',
-      moduleTitle: 'Test module',
-      launchPage: 'index.html',
-      fileName: 'imsmanifest.xml'
-    }))
-    .pipe(gulp.dest('course'))
+  gulp.src('course/**')
+  .pipe(manifest({
+    version: '1.2',
+    courseId: 'Test course',
+    SCOtitle: 'Test module',
+    moduleTitle: 'Test module',
+    launchPage: 'index.html',
+    fileName: 'imsmanifest.xml'
+  }))
+  .pipe(gulp.dest('course'))
+
+  .on('end',function(){
+  gulp.run('zip');
+  })
+});
+
+gulp.task('zip', function() {
+  return gulp.src('course/*')
+  .pipe(zip('scorm.zip'))
+  .pipe(gulp.dest('SCORM'));
 });
